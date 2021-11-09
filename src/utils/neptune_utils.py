d53c1eb50809e410d991acbf8d522f3889dcff4d
mayu-ot <otani_mayu@cyberagent.co.jp>
2021-11-09 22:38:11 +0900

    neptune result downlader

--- src/utils/neptune_utils.py
+++ src/utils/neptune_utils.py
@@ -1,12 +1,14 @@
+import tempfile
 import neptune.new as neptune
 import os
 from data.conf.model_cfg import HPARAM_RUNS
+import mmcv
 
 
-def load_hparam_neptune(model_cfg, alpha=0.5, beta=0.4):
-    if model_cfg not in HPARAM_RUNS[f"alpha={alpha},beta={beta}"]:
+def load_hparam_neptune(model_cfg, key):
+    if model_cfg not in HPARAM_RUNS[key]:
         return []
-    run_id = HPARAM_RUNS[f"alpha={alpha},beta={beta}"][model_cfg]
+    run_id = HPARAM_RUNS[key][model_cfg]
     run = neptune.init(
         project=os.environ["NEPTUNE_PROJECT"], run=run_id, mode="read-only"
     )
@@ -26,4 +28,8 @@ def load_results(run_id, model_name):
     run = neptune.init(
         project=os.environ["NEPTUNE_PROJECT"], run=run_id, mode="read-only"
     )
-    run[""]
+    with tempfile.TemporaryDirectory() as tmpdirname:
+        run[f"results/{model_name}"].download(tmpdirname)
+        results = mmcv.load(os.path.join(tmpdirname, f"{model_name}.pkl"))
+    run.stop()
+    return results
