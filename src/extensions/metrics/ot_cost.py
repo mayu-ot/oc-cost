089b9e5937eb6bae05241bedad351a085083ba8e
mayu-ot <otani_mayu@cyberagent.co.jp>
2022-05-15 14:53:12 +0900

    add typing

--- src/extensions/metrics/ot_cost.py
+++ src/extensions/metrics/ot_cost.py
@@ -1,22 +1,32 @@
+from typing import Callable, Sequence, Tuple, Callable, Union
 from jupyter_bbox_widget import bbox
 from mmdet.core.evaluation.bbox_overlaps import bbox_overlaps
 import ot
 import numpy as np
-import pdb
+import numpy.typing as npt
 
 
-def get_bbox_overlaps(bboxes1, bboxes2, mode="iou", eps=1e-6):
+def get_bbox_overlaps(
+    bboxes1: npt.ArrayLike, bboxes2: npt.ArrayLike, mode: str = "iou", eps: float = 1e-6
+):
     if mode == "iou":
         return bbox_overlaps(bboxes1, bboxes2, eps=eps)
     elif mode == "giou":
         return bbox_gious(bboxes1, bboxes2, eps=eps)
+    else:
+        raise RuntimeError("valid mode is iou or giou")
 
 
-def bbox_gious(bboxes1, bboxes2, eps=1e-6, use_legacy_coordinate=False):
-    """Calculate the ious between each bbox of bboxes1 and bboxes2.
+def bbox_gious(
+    bboxes1: npt.ArrayLike,
+    bboxes2: npt.ArrayLike,
+    eps: float = 1e-6,
+    use_legacy_coordinate: bool = False,
+) -> npt.ArrayLike:
+    """Calculate the generalized ious between each bbox of bboxes1 and bboxes2.
     Args:
-        bboxes1 (ndarray): Shape (n, 4)
-        bboxes2 (ndarray): Shape (k, 4)
+        bboxes1 (ndarray): Shape (n, 4) # [[x1, y1, x2, y2], ...]
+        bboxes2 (ndarray): Shape (k, 4) # [[x1, y1, x2, y2], ...]
         use_legacy_coordinate (bool): Whether to use coordinate system in
             mmdet v1.x. which means width, height should be
             calculated as 'x2 - x1 + 1` and 'y2 - y1 + 1' respectively.
@@ -32,6 +42,7 @@ def bbox_gious(bboxes1, bboxes2, eps=1e-6, use_legacy_coordinate=False):
         extra_length = 0.0
     else:
         extra_length = 1.0
+
     bboxes1 = bboxes1.astype(np.float32)
     bboxes2 = bboxes2.astype(np.float32)
     rows = bboxes1.shape[0]
@@ -78,16 +89,25 @@ def bbox_gious(bboxes1, bboxes2, eps=1e-6, use_legacy_coordinate=False):
     return gious
 
 
-def add_label(result):
+def add_label(result: Sequence[Sequence]) -> npt.ArrayLike:
     labels = [[i] * len(r) for i, r in enumerate(result)]
     labels = np.hstack(labels)
     return np.hstack([np.vstack(result), labels[:, None]])
 
 
 def cost_func(x, y, mode: str = "giou", alpha: float = 0.8):
-    giou_val = get_bbox_overlaps(
-        x[:4][None, :], y[:4][None, :], mode
-    )  # range [-1, 1]
+    """Calculate a unit cost
+
+    Args:
+        x (np.ndarray): a detection [x1, y1, x2, y2, s, l]. s is a confidence value, and l is a classification label.
+        y (np.ndarray): a detection [x1, y1, x2, y2, s, l]. s is a confidence value, and l is a classification label.
+        mode (str, optional): Type of IoUs. Defaults to "giou" (Generalized IoU).
+        alpha (float, optional): weights to balance localization and classification errors. Defaults to 0.8.
+
+    Returns:
+        float: a unit cost
+    """
+    giou_val = get_bbox_overlaps(x[:4][None, :], y[:4][None, :], mode)  # range [-1, 1]
     loc_cost = 1 - (giou_val + 1) * 0.5  # normalized to [0, 1]
     l_x, l_y = x[-1], y[-1]
     if l_x == l_y:
@@ -99,8 +119,14 @@ def cost_func(x, y, mode: str = "giou", alpha: float = 0.8):
     return alpha * loc_cost + (1 - alpha) * cls_cost
 
 
-def get_cmap(a_result, b_result, alpha=0.8, beta=0.4, mode="giou"):
-    """[summary]
+def get_cmap(
+    a_result: Sequence[npt.ArrayLike],
+    b_result: Sequence[npt.ArrayLike],
+    alpha: float = 0.8,
+    beta: float = 0.4,
+    mode="giou",
+) -> Tuple[npt.ArrayLike]:
+    """Calculate cost matrix
 
     Args:
         a_result ([type]): detections
@@ -109,7 +135,7 @@ def get_cmap(a_result, b_result, alpha=0.8, beta=0.4, mode="giou"):
 
     Returns:
         dist_a (np.array): (N+1,) array. distribution over detections.
-        dist_b (np.array): (M,) array. distribution over detections.
+        dist_b (np.array): (M+1,) array. distribution over detections.
         cost_map:
     """
     a_result = add_label(a_result)
@@ -126,19 +152,15 @@ def get_cmap(a_result, b_result, alpha=0.8, beta=0.4, mode="giou"):
     dist_b = np.ones(m + 1)
 
     # cost for dummy demander / supplier
-    # if use_dummy:
     cost_map[-1, :] = beta
     cost_map[:, -1] = beta
     dist_a[-1] = m
     dist_b[-1] = n
 
-    # dist_a /= dist_a.sum()
-    # dist_b /= dist_b.sum()
-
     return dist_a, dist_b, cost_map
 
 
-def postprocess(M, total_cost, log):
+def postprocess(M: npt.ArrayLike, total_cost: float, log: dict) -> float:
     G = log["G"]
     G[-1, -1] = 0
     G /= G.sum()
@@ -146,7 +168,12 @@ def postprocess(M, total_cost, log):
     return total_cost
 
 
-def get_ot_cost(a_detection, b_detection, cmap_func, return_matrix=False):
+def get_ot_cost(
+    a_detection: list,
+    b_detection: list,
+    cmap_func: Callable,
+    return_matrix: bool = False,
+) -> Union[float, Tuple[float, dict]]:
     """[summary]
 
     Args:
@@ -165,13 +192,11 @@ def get_ot_cost(a_detection, b_detection, cmap_func, return_matrix=False):
     a, b, M = cmap_func(a_detection, b_detection)
     total_cost, log = ot.emd2(a, b, M, return_matrix=True)
     total_cost = postprocess(M, total_cost, log)
-    log["M"] = M
-    log["a"] = a
-    log["b"] = b
-
-    # total_cost = 2 / (1 + np.exp(-total_cost)) - 1  # normalize [0, 1]
 
     if return_matrix:
+        log["M"] = M
+        log["a"] = a
+        log["b"] = b
         return total_cost, log
     else:
         return total_cost
